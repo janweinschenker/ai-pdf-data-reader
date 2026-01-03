028f2521886401a29c385813bbcaa5a85c677e3f
Jan Weinschenker <jan.weinschenker@holisticon.de>
2026-01-03 18:37:35 +0100

    refactor: removed unused import

--- src/main/java/org/weinschenker/tldrdatareader/application/port/out/AiModelOutPort.java
+++ src/main/java/org/weinschenker/tldrdatareader/application/port/out/AiModelOutPort.java
@@ -1,11 +1,11 @@
 package org.weinschenker.tldrdatareader.application.port.out;
 
 import org.weinschenker.tldrdatareader.domain.PartList;
-import org.weinschenker.tldrdatareader.domain.PdfContainer;
 
 import java.util.Optional;
 
 public interface AiModelOutPort {
+
     Optional<PartList> extractStructuredData(String textFromJpeg);
 
     Optional<PartList> extractStructuredData(String fileContent, String jsonSchema);
